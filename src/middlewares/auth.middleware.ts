0279d9553c7243710e1c86e190e9d98413077205
Joy <joyprakash.dey@elvirainfotech.com>
2025-10-15 18:07:02 +0530

    x-api-key enabled

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -3,19 +3,19 @@ import { AppError } from './errorHandler'
 import bcrypt from 'bcrypt'
 
 export const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
-  // const plainTextKey = req.headers['x-api-key'] as string
-  // const hashedKey = process.env.API_KEY_HASH
+  const plainTextKey = req.headers['x-api-key'] as string
+  const hashedKey = process.env.API_KEY_HASH
 
-  // if (!plainTextKey || !hashedKey) {
-  //   throw new AppError('Unauthorized: API key is missing', 401)
-  // }
+  if (!plainTextKey || !hashedKey) {
+    throw new AppError('Unauthorized: API key is missing', 401)
+  }
 
   // // Use bcrypt to securely compare the plain-text key with the stored hash
-  // const isValid = await bcrypt.compare(plainTextKey, hashedKey)
+  const isValid = await bcrypt.compare(plainTextKey, hashedKey)
 
-  // if (!isValid) {
-  //   throw new AppError('Unauthorized: Invalid API key', 401)
-  // }
+  if (!isValid) {
+    throw new AppError('Unauthorized: Invalid API key', 401)
+  }
 
   // If the key is valid, proceed to the next handler
   next()
