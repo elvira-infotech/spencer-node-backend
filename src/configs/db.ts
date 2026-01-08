37463ed127f6b416580d8712921a4c62536493c5
Joy <joyprakash.dey@elvirainfotech.com>
2026-01-08 20:50:32 +0530

    updating prisma object creation

--- src/configs/db.ts
+++ src/configs/db.ts
@@ -1,9 +1,16 @@
 import { PrismaClient } from '../prisma/generated/client'
+import { PrismaNeon } from '@prisma/adapter-neon'
+
+const adapter = new PrismaNeon({
+  connectionString: process.env.DATABASE_URL!,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+})
 
 // This creates a single, reusable instance of the PrismaClient.
 const prisma = new PrismaClient({
-  log: ['query', 'info', 'warn', 'error'],
-  accelerateUrl: process.env.DATABASE_URL,
+  adapter,
 })
 
 export default prisma
